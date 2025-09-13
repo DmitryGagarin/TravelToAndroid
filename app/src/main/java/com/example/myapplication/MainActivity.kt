a2bb7cdfd91751aa87df8af5c72707657e988568
Dmitrii Gagarin <gagriu@yandex.ru>
2025-09-13 16:04:14 +0300

    navigation

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,152 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                val navController = rememberNavController()
+                Surface(color = Color.White) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController)
+                        }, content = { padding ->
+                            NavHostContainer(navController = navController, padding = padding)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+data class BottomNavItem(
+    val label: String,
+    val icon: ImageVector,
+    val route: String
+)
+
+object Constants {
+    val BottomNavItems = listOf(
+        BottomNavItem(
+            label = "Places",
+            icon = Icons.Filled.Place,
+            route = "place"
+        ),
+        BottomNavItem(
+            label = "Liked",
+            icon = Icons.Filled.ThumbUp,
+            route = "like"
+        ),
+        BottomNavItem(
+            label = "Account",
+            icon = Icons.Filled.Person,
+            route = "profile"
+        )
+    )
+}
+
+@Composable
+fun NavHostContainer(
+    navController: NavHostController,
+    padding: PaddingValues
+) {
+    NavHost(
+        navController = navController,
+
+        // set the start destination as home
+        startDestination = "place",
+
+        // Set the padding provided by scaffold
+        modifier = Modifier.padding(paddingValues = padding),
+
+        builder = {
+            // route : Home
+            composable("place") {
+                PlaceScreen()
+            }
+            // route : search
+            composable("like") {
+                LikeScreen()
+            }
+            // route : profile
+            composable("profile") {
+                ProfileScreen()
+            }
+        })
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    NavigationBar(
+        // set background color
+        containerColor = Color(0xFF0F9D58)
+    ) {
+        // observe the backstack
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+        // observe current route to change the icon
+        // color,label color when navigated
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        // Bottom nav items we declared
+        Constants.BottomNavItems.forEach { navItem ->
+
+            // Place the bottom nav items
+            NavigationBarItem(
+                // it currentRoute is equal then its selected route
+                selected = currentRoute == navItem.route,
+                // navigate on click
+                onClick = {
+                    navController.navigate(navItem.route)
+                },
+                // Icon of navItem
+                icon = {
+                    Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                },
+                // label
+                label = {
+                    Text(text = navItem.label)
+                },
+                alwaysShowLabel = false,
+
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.White, // Icon color when selected
+                    unselectedIconColor = Color.White, // Icon color when not selected
+                    selectedTextColor = Color.White, // Label color when selected
+                    indicatorColor = Color(0xFF195334) // Highlight color for selected item
+                )
+            )
+        }
+    }
+}
